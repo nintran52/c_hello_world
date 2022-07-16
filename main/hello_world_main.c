48d034805325d0d2bd94621e85c0a5a7ea0aba0e
Nin Tran <nintran52@Nins-MacBook-Pro.local>
2022-07-17 06:51:06 +0700

    Compoment input

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -11,11 +11,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
+// #include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "output_iot.h"
+// #include "input_iot.h"
 
 TimerHandle_t xTimers[2];
+// EventGroupHandle_t xEventGroup;
+
+// #define BIT_EVENT_BUTTON_PRESS (1 << 0)
+// #define BIT_EVENT_BUTTON_DOUBLE_PRESS (1 << 1)
 
 void vTimerCallback(TimerHandle_t xTimer)
 {
@@ -59,15 +65,52 @@ void vTask3(void *pvParameters)
     }
 }
 
+// void vTaskButton(void *pvParameters)
+// {
+//     for (;;)
+//     {
+//         EventBits_t uxBits = xEventGroupWaitBits(
+//             xEventGroup,
+//             BIT_EVENT_BUTTON_PRESS | BIT_EVENT_BUTTON_DOUBLE_PRESS,
+//             pdTRUE,
+//             pdFALSE,
+//             portMAX_DELAY);
+
+//         if (uxBits & BIT_EVENT_BUTTON_PRESS)
+//         {
+//             printf("BUTTON PRESS\n");
+//             output_io_toggle(15);
+//         }
+//         if (uxBits & BIT_EVENT_BUTTON_DOUBLE_PRESS)
+//         {
+//             printf("BUTTON DOUBLE PRESS\n");
+//             output_io_toggle(13);
+//         }
+//     }
+// }
+
+// void button_callback(int pin)
+// {
+//     if (pin == GPIO_NUM_0)
+//     {
+//         BaseType_t pxHigherPriorityTaskWoken;
+//         xEventGroupSetBitsFromISR(xEventGroup, BIT_EVENT_BUTTON_PRESS, &pxHigherPriorityTaskWoken);
+//     }
+// }
+
 void app_main(void)
 {
     xTimers[0] = xTimerCreate("TimerBlink", pdMS_TO_TICKS(500), pdTRUE, (void *)0, vTimerCallback);
     xTimers[1] = xTimerCreate("TimerPrint", pdMS_TO_TICKS(1000), pdTRUE, (void *)1, vTimerCallback);
 
     output_io_create(13);
+    // input_io_create(GPIO_NUM_0, ANY_EDGE);
+    // input_set_callback(button_callback);
 
     xTimerStart(xTimers[0], 0);
     xTimerStart(xTimers[1], 0);
+
+    // xEventGroup = xEventGroupCreate();
 
     xTaskCreate(
         vTask1,   /* Function that implements the task. */
@@ -91,4 +134,11 @@ void app_main(void)
         NULL,     /* Parameter passed into the task. */
         6,        /* Priority at which the task is created. */
         NULL);    /* Used to pass out the created task's handle. */
+    // xTaskCreate(
+    //     vTaskButton,   /* Function that implements the task. */
+    //     "vTaskButton", /* Text name for the task. */
+    //     1024 * 2,      /* Stack size in words, not bytes. */
+    //     NULL,          /* Parameter passed into the task. */
+    //     7,             /* Priority at which the task is created. */
+    //     NULL);         /* Used to pass out the created task's handle. */
 }
