bf4ae40229be134580a3701cfbde35950fac6254
Nin Tran <nintran52@Nins-MacBook-Pro.local>
2022-07-16 21:56:28 +0700

    Component, Timers and Tasks

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -10,34 +10,85 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "output_iot.h"
+
+TimerHandle_t xTimers[2];
+
+void vTimerCallback(TimerHandle_t xTimer)
+{
+    /* Optionally do something if the pxTimer parameter is NULL. */
+    configASSERT(xTimer);
+    int ulCount = (uint32_t)pvTimerGetTimerID(xTimer);
+    if (ulCount == 0)
+    {
+        output_io_toggle(13);
+    }
+    else if (ulCount == 1)
+    {
+        printf("Hello\n");
+    }
+}
+
+void vTask1(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task 1\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void vTask2(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task 2\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void vTask3(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task 3\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    xTimers[0] = xTimerCreate("TimerBlink", pdMS_TO_TICKS(500), pdTRUE, (void *)0, vTimerCallback);
+    xTimers[1] = xTimerCreate("TimerPrint", pdMS_TO_TICKS(1000), pdTRUE, (void *)1, vTimerCallback);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    output_io_create(13);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    xTimerStart(xTimers[0], 0);
+    xTimerStart(xTimers[1], 0);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    xTaskCreate(
+        vTask1,   /* Function that implements the task. */
+        "vTask1", /* Text name for the task. */
+        1024,     /* Stack size in words, not bytes. */
+        NULL,     /* Parameter passed into the task. */
+        4,        /* Priority at which the task is created. */
+        NULL);    /* Used to pass out the created task's handle. */
 
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    xTaskCreate(
+        vTask2,   /* Function that implements the task. */
+        "vTask2", /* Text name for the task. */
+        1024,     /* Stack size in words, not bytes. */
+        NULL,     /* Parameter passed into the task. */
+        5,        /* Priority at which the task is created. */
+        NULL);    /* Used to pass out the created task's handle. */
+    xTaskCreate(
+        vTask3,   /* Function that implements the task. */
+        "vTask3", /* Text name for the task. */
+        1024,     /* Stack size in words, not bytes. */
+        NULL,     /* Parameter passed into the task. */
+        6,        /* Priority at which the task is created. */
+        NULL);    /* Used to pass out the created task's handle. */
 }
